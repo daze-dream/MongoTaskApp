3bef24eec162a10e1fcdc992e8b9be58512b511c
CheetoDustNeo <misterrolypoly@gmail.com>
2021-09-29 13:20:52 -0500

    renamed app startpoint

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+require('./db/mongoose') 
+const User = require('./models/user.js')
+const Task = require('./models/task.js')
+//----------------------------------------------
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    console.log(req.body);
+    const user = new User(req.body);
+    user.save().then(() => {
+        console.log('Successful save from request: ', req.body)
+        res.send(user);
+    }).catch((e) => {
+        console.log('Error from request: ', req.body)
+        res.status(400).send(e);
+    })
+    
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+    task.save().then(()=> {
+        console.log('Successful save from request: ', req.body)
+        res.send(task);
+    }).catch((e)=> {
+        console.log('Error from request: ', req.body)
+        res.status(400).send(e);
+    })
+    
+})
+
+app.listen( port, () => {
+    console.log('listening on port ' + port);
+})
