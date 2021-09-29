aeeedda2ed6d4c41583ac4dedaf978d3fc04b515
CheetoDustNeo <misterrolypoly@gmail.com>
2021-09-29 15:00:39 -0500

    single and multi task REST done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 require('./db/mongoose') 
 const User = require('./models/user.js')
-const Task = require('./models/task.js')
+const Task = require('./models/task.js');
+const { Mongoose } = require('mongoose');
+const { ObjectID } = require('bson');
 //----------------------------------------------
 
 const app = express();
@@ -34,8 +36,64 @@ app.post('/tasks', (req, res) => {
     
 })
 
+
+
 app.get('/users', (req, res) => {
-    
+    User.find({}).then((users) => {
+        console.log('successful querying of all users');
+        res.send(users);
+    }).catch((e) => {
+        console.log('internal server error', e);
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+    console.log(ObjectID('615380b18a19e5d3ac4d66ab'));
+    User.findById(_id).then((user) => {
+        if(!user){
+            console.log('Bad user search by ID: ', _id);
+            return res.status(404).send('No user found');
+        }
+        res.send(user);
+    }).catch((e) => {
+        if(e.name === 'CastError'){
+            return res.status(400).send('Invalid id')
+        }
+        console.log('internal server error', e);
+        res.status(500).send(e)
+    });
+    console.log(req.params);
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        console.log('successful querying of all tasks');
+        res.send(tasks);
+    }).catch((e) => {
+        console.log('internal server error', e);
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+    Task.findById(_id).then((task) => {
+        if(!task){
+            console.log('Bad task search by ID:', _id);
+            return res.status(404).send('No user found');
+        }
+        console.log('successful single task query of id', _id)
+        res.send(task);
+    }).catch((e) => {
+        if(e.name === 'CastError'){
+            return res.status(400).send('Invalid id')
+        }
+        console.log('internal server error', e);
+        res.status(500).send(e)
+    });
+    console.log(req.params);
 })
 
 app.listen( port, () => {
