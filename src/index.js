2b73696c2a992aa5f3ef0e8633ce45f126876dc9
CheetoDustNeo <misterrolypoly@gmail.com>
2021-10-01 21:02:06 -0500

    cleaned up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 require('./db/mongoose') 
-const User = require('./models/user.js')
-const Task = require('./models/task.js');
 const userRouter = require('./routers/user_router')
 const taskRouter = require('./routers/task_router')
 const { Mongoose } = require('mongoose');
@@ -16,11 +14,18 @@ const port = process.env.PORT || 3000;
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
-
-
-
-
-
 app.listen( port, () => {
     console.log('listening on port ' + port);
 })
+
+
+
+// const foo = async () => {
+//     const password = 'kitten22';
+//     const hashedPass = await bcrypt.hash(password, 8);
+//     console.log(hashedPass);
+//     const isMatch = await bcrypt.compare('kitten22', hashedPass);
+//     console.log(isMatch);
+// }
+
+// foo()
